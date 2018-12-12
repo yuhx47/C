072dadce40956f29dc5b25bebb3a9e060abfb251
Clutch <37691985+Clutch47@users.noreply.github.com>
2018-12-12 10:47:02 +0900

    计算区间频率

--- PPMS_freq.c
+++ PPMS_freq.c
@@ -3,25 +3,22 @@
 #include<stdlib.h>
 
 int cnt = 0;
-float score[32245];
+int cntm = 0;
+float score[33566];
 char buf[100];
-float d1 = 0.4;
-float d2 = 0.41;
+float d1 = 0.35;
+float d2 = 0.36;
 int f = 0;
 float k;
 FILE *fp;
-int main() {
-	fp = fopen ("noid_vv5_PPMS_order.txt", "r");
+int main(int argc, char *argv[]) {
+	fp = fopen (argv[1], "r");
 	while (fgets(buf, sizeof(buf), fp)!= NULL) {
-		sscanf (buf, "%f", score + cnt);
-//		printf ("%d\n", r_dist[cnt]);
-		cnt++;
+		sscanf (buf, "%f", score + cntm);
+		cntm++;
 		}
-/*	for (cnt = 0; cnt < 29898; cnt++) {
-		printf("%d\n", r_dist[cnt]);
-	}*/	
-	while (d1 <= 0.60) { 
-		for (cnt = 0; cnt < 32245; cnt++) {	
+	while (d1 <= 0.65) { 
+		for (cnt = 0; cnt < cntm; cnt++) {	
 			if (score[cnt] >= d1 && score[cnt] > d2) {
 				f = 0;
 				printf("%f\t%f\t%d\n", d1, d2, f);
@@ -30,10 +27,14 @@ int main() {
 			else if (score[cnt] >= d1 && score[cnt] <= d2) {
                                 f++;
 		//		printf("%d\n", f);
-				if (score[cnt + 1] > d1 && score[cnt + 1] > d2) {
+				if (score[cnt + 1] > d1 && score[cnt + 1] >= d2) {
 					printf("%f\t%f\t%d\n", d1, d2, f);
 					break;
 				}
+				else if (cnt + 1 == cntm) {
+					printf("%f\t%f\t%d\n", d1, d2, f);
+                                        break;
+                                }
 			}
 		}
 		d1 = d1 + 0.01;
